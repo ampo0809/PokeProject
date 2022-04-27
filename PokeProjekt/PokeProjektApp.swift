9f3915849d943eb3187cf1d4738bdc722845d7bb
ampo0809 <ampuero0809@gmail.com>
2022-04-27 12:47:00 +0200

    Initial Commit

---
+++ PokeProjekt/PokeProjektApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeProjektApp.swift
+//  PokeProjekt
+//
+//  Created by Nacho on 27.04.22.
+//
+
+import SwiftUI
+
+@main
+struct PokeProjektApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
