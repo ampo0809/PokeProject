e1af77db1a18f89b1ddc60fc37b17dca8d7ba72f
ampo0809 <ampuero0809@gmail.com>
2022-06-09 14:26:53 +0300

    Change pokemon list to external .txt file

--- PokeProjekt/Model/Local/PokeDataController.swift
+++ PokeProjekt/Model/Local/PokeDataController.swift
@@ -9,13 +9,29 @@ import UIKit
 
 class PokeDataController: ObservableObject {
     
+    fileprivate var allPokemonNames = [String]()
     @Published var pokeData: [PokeDataLocal] = []
     
-    init() { loadPokeData() }
+    init() {
+        populateAllPokemonNamesArray()
+        loadPokeData()
+    }
+    
+    // Reads all the pokémon names from an external .txt file and populates an array
+    private func populateAllPokemonNamesArray() {
+        
+        if let allNamesURL = Bundle.main.url(forResource: "PokemonNameList", withExtension: ".txt") {
+            if let allNames = try? String(contentsOf: allNamesURL) {
+                allPokemonNames = allNames.components(separatedBy: "\n")
+            }}
+        if allPokemonNames.isEmpty {
+            allPokemonNames = ["pikachu"]
+        }
+    }
     
     private func loadPokeData() {
         
-        for name in NamesArray.names {
+        for name in allPokemonNames {
             pokeData.append( PokeDataLocal(name: name))
         }
     }
